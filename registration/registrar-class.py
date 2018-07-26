13880538fba1a7c7e7703d07330254ec28fae8f0
lh20180514 <18629005576@163.com>
2018-07-26 18:02:15 +0800

    挂号

--- registration/registrar-class.py
+++ registration/registrar-class.py
@@ -1,89 +1,157 @@
 import requests
 import json
+import copy
+import time
 
 class Registrar:
+    headers = {
+        'Connection': 'keep - alive',  # 保持链接状态
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+        }
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'}
+    def __init__(self, doctorId, patienter, cookieStr):
+        self.doctorId = doctorId
+        self.patienter = patienter
+        self.cookies = self.transCookie(cookieStr)
 
-cookie_str = r'__RequestVerificationToken=64TGmcVMXnI--UxgGh5mK4QCmxsRlh7TBPjoeaIXlllknmbY916toJW5NRwkpRWN9H8Kn4xtP-4cu-FSpYRe5YbpiiUOsMB6CcPhnhEkZfo1; ASP.NET_SessionId=drnw1myaweyjaf4j1tudmxbm; ' \
-             r'User=UserName=lh8229238&Password=lh301415926; ' \
-             r'HBHOSPITALCODE=0018'
-#把cookie字符串处理成字典，以便接下来使用
-cookies = {}
-for line in cookie_str.split(';'):
-    key, value = line.split('=', 1)
-    cookies[key] = value
+    # 字符串cookie转换为字典类型
+    def transCookie(self, cookieStr):
+        cookies = {}
+        for line in cookieStr.split(';'):
+            # split 1表示分隔的次数为1次，
+            # 存在User=UserName=lh8229238，分隔1次为(User,UserName=lh8229238)
+            key, value = line.split('=', 1)
+            cookies[key] = value
+        return cookies
 
-# data = {'DoctorId': '6101000210003001', 'registType': '1'}
+    # 得到该医生的挂号ID和日期，对于抢号来说默认取一个即可
+    def getRegistrarId(self):
+        registrar = {}
+        formData = {'DoctorId': self.doctorId, 'registType': '1'}
 
-#得到该医生的挂号ID和日期
-def get_registrar_id(doctor_id):
-    registrar = {}
-    data = {'DoctorId': doctor_id ,
-            'registType':'1'}
-    response = requests.post('http://www.dyyy120.com/Clinic/GHGetClinicRegistList',data=data,headers=headers)
-    jsonResult = json.loads(response.text)
-    for registModel in jsonResult['RegistModelList']:
-        if registModel['Status'] == 1:
-            registrar = registModel
-            break
-    return registrar
+        start = time.clock()
+        response = requests.post('http://www.dyyy120.com/Clinic/GHGetClinicRegistList',
+                                 data=formData, headers=self.headers)
+        end = time.clock()
+        print('GHGetClinicRegistList Running time: %s Seconds' % (end - start))
 
-# 6101000210003322
-# print(get_registrar_id('6101000210003001'))
+        if response.status_code == 200:
+            # json.dumps  将Python对象编码成JSON字符串
+            # json.loads  将已编码的JSON字符串解码为Python对象
+            # json.loads(response.text)等同 response.json()
+            jsonResult = json.loads(response.text)
+            for registModel in jsonResult['RegistModelList']:
+                if registModel['Status'] == 1:
+                    registrar = registModel
+                    break
+        else:
+            print('getRegistrarId 请求错误: ' + response.text)
+        return registrar
 
-#准备挂号的数据：医生、时间、病人
-def construction_registration_data(doctor_id,patienter):
-    # registrar = {'ClinicDate': '2018-07-30', 'TimePartType': 0, 'Status': 1, 'ClinicLabelId': '6101000210003322', 'ClinicLabelType': 1, 'RsvModel': 1, 'BookedNums': 15, 'CountLimit': 30}
-    registrar = get_registrar_id(doctor_id)
+    # 准备挂号的数据：医生、时间、病人
+    def constructionRegistrationData(self):
+        # registrar = {'ClinicDate': '2018-07-30', 'TimePartType': 0, 'Status': 1, 'ClinicLabelId': '6101000210003322', 'ClinicLabelType': 1, 'RsvModel': 1, 'BookedNums': 15, 'CountLimit': 30}
+        registrationData = {}
+        registrar = self.getRegistrarId()
 
-    if(any(registrar) == False):
-        print("还没开始construction_registration_data")
-        return {}
+        if any(registrar) == False:
+            print("没有获取到医生的挂号ID，可能还没有放号！")
+            return registrationData
 
-    form_data = {'clinicLabelId':registrar.get('ClinicLabelId'),
-                 'timePartType':registrar.get('TimePartType'),
-                 'clinicDate': registrar.get('ClinicDate')}
-    response = requests.post('http://www.dyyy120.com/Clinic/GetBookSureModel', data=form_data, headers=headers,cookies = cookies)
-    jsonResult = response.json()
-    patientCards = json.loads(jsonResult['PatientCardJsons'])
+        formData = {'clinicLabelId': registrar.get('ClinicLabelId'),
+                     'timePartType': registrar.get('TimePartType'),
+                     'clinicDate': registrar.get('ClinicDate')}
 
-    for personTmp in patientCards:
-        if(personTmp['TrueName'] == patienter):
-            person = personTmp
-            break
+        start = time.clock()
+        response = requests.post('http://www.dyyy120.com/Clinic/GetBookSureModel', data=formData, headers=self.headers,
+                                 cookies=self.cookies)
+        end = time.clock()
+        print('GetBookSureModel Running time: %s Seconds' % (end - start))
 
-    registration_data = {
-                'param':'orderAPI',
-                'hospitalId':'61010002',
-                'patientId':person['PersonId'],
-                'clinicLabelId':jsonResult['ClinicLabelId'],
-                'clinicDate':jsonResult['ClinicDate'],
-                'timePartType':str(jsonResult['TimePartType']),
-                'timePart':jsonResult['TimePartList'][-1]['TimePart'],
-                'channcelType':str(jsonResult['ChannelTypeId']),
-                'rsvmodel':str(jsonResult['RsvModel']),
-                'returnVisitId':'2',
-                'symptom':' ',
-                'cardnum':person['CardList'][-1]['CardNum'],
-                'payChanel':'2'
-                }
-    return registration_data
+        if response.status_code == 200:
+            # json.dumps  将Python对象编码成JSON字符串
+            # json.loads  将已编码的JSON字符串解码为Python对象
+            # json.loads(response.text)等同 response.json()
+            jsonResult = json.loads(response.text)
 
-print(construction_registration_data('6101000210027013','刘辉'))
+            if jsonResult['ReturnInfo']['Result'] == 2:
+                print('constructionRegistrationData :' + jsonResult['ReturnInfo']['ResultMsg'])
+                return registrationData
 
-# print(construction_registration_data('6101000210011004','刘辉'))
+            patientCards = json.loads(jsonResult['PatientCardJsons'])
 
-def start_registration(doctor_id,patienter):
-    from_data = construction_registration_data(doctor_id, patienter)
-    print(from_data)
-    if(any(from_data) == False):
-        print("还没开始start_registration")
-        return
-    response = requests.post('http://www.dyyy120.com/ajax.aspx', data=from_data, headers=headers,
-                             cookies=cookies)
-    print(response.json())
+            # 得到挂号病人的详细信息
+            for personTmp in patientCards:
+                if personTmp['TrueName'] == self.patienter:
+                    person = personTmp
+                    break
 
-# start_registration('6101000210011004', '刘辉')
+            if any(person):
+                registrationData = {'param': 'orderAPI',
+                                    'hospitalId': '61010002',
+                                    'patientId': person['PersonId'],
+                                    'clinicLabelId': jsonResult['ClinicLabelId'],
+                                    'clinicDate': jsonResult['ClinicDate'],
+                                    'timePartType': str(jsonResult['TimePartType']),
+                                    # 'timePart': jsonResult['TimePartList'][-1]['TimePart'],
+                                    'TimePartList': jsonResult['TimePartList'],
+                                    'channcelType': str(jsonResult['ChannelTypeId']),
+                                    'rsvmodel': str(jsonResult['RsvModel']),
+                                    'returnVisitId': '2',
+                                    'symptom': ' ',
+                                    'cardnum': person['CardList'][-1]['CardNum'],
+                                    'payChanel': '2'}
+            else:
+                print("没有找到挂号的病人")
+        else:
+            print('constructionRegistrationData 请求错误: ' + response.text)
+        return registrationData
+
+    #开始发起挂号请求
+    def startRegistration(slef):
+        formData = slef.constructionRegistrationData()
+        # 构造挂号数据为空
+        if any(formData) == False:
+            print("构造的挂号数据为空！")
+        else:
+            timePartList = copy.deepcopy(formData['TimePartList'])
+            # 挂号时间采用优先逆序
+            for timePart in timePartList[::-1]:
+                formData['TimePart'] = timePart['TimePart']
+                if 'TimePartList' in formData.keys():
+                    formData.pop('TimePartList')
+
+                start = time.clock()
+                response = requests.post('http://www.dyyy120.com/ajax.aspx', data=formData,
+                                         headers=slef.headers, cookies=slef.cookies)
+                end = time.clock()
+                print('ajax.aspx Running time: %s Seconds' % (end - start))
+
+                if response.status_code == 200:
+                    jsonResult = json.loads(response.text)
+                    if jsonResult['ReturnCode'] == 0:
+                        print(jsonResult['Message'])
+                    else:
+                        print(jsonResult['Message'])
+                    break
+                else:
+                    print('startRegistration 请求错误: ' + response.text)
+
+if __name__ == '__main__':
+    start = time.clock()
+
+    doctorId = r'6101000210003003'
+    patienter = r'刘若熙'
+    cookieStr = r'__RequestVerificationToken=64TGmcVMXnI--UxgGh5mK4QCmxsRlh7TBPjoeaIXlllknmbY916toJW5NRwkpRWN9H8Kn4xtP-4cu-FSpYRe5YbpiiUOsMB6CcPhnhEkZfo1; ' \
+                r'ASP.NET_SessionId=drnw1myaweyjaf4j1tudmxbm; ' \
+                r'User=UserName=lh8229238&Password=lh301415926; ' \
+                r'HBHOSPITALCODE=5912'
+    registrar = Registrar(doctorId, patienter, cookieStr)
+    # print(registrar.getRegistrarId())
+    # print(json.dumps(registrar.constructionRegistrationData())
+    registrar.startRegistration()
+
+    end = time.clock()
+    print('main Running time: %s Seconds' % (end - start))
 
 
